0ca50f5566b6fefe61330dc574f5547deac65d0e
Pradhapan M S <pradhap290@gmail.com>
2025-01-11 23:42:36 +0530

    First Commit V1

---
+++ src/config/gemini.js
@@ -0,0 +1,37 @@
+const apiKey = "AIzaSyDvk_UnCHkwRU9MA43sP35BzNI8nJWMcbw";
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash-exp",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    const response = result.response;
+    console.log(result.response.text());
+    return response.text();
+  }
+  
+  export default run;
